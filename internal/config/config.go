3869ee3617a6b10499d7e452075bc83caba7d66b
Yi donghoon <icq4ever@gmail.com>
2025-09-27 05:54:38 +0900

    keys location updated

---
+++ internal/config/config.go
@@ -0,0 +1,9 @@
+package config
+
+const (
+	IssuerPrivDefault = "keys/issuer/privkey.pem"
+	IssuerPubDefault  = "keys/issuer/pubkey.pem"
+
+	ProvPrivDefault = "keys/provisioner/privkey.pem"
+	ProvPubDefault  = "keys/provisioner/pubkey.pem"
+)
